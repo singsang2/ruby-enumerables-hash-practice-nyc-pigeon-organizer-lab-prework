e754a7c57920215b64bab43ce4da365f72932362
Sung Bae <singsang2@gmail.com>
2020-04-15 15:20:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = Hash.new
+  data.each do |trait, description|
+    description.each do |description, names|
+      names.each do |name|
+        pigeon_list[name] = {:color => [], :gender => [], :lives => []} if pigeon_list[name] == nil
+        pigeon_list[name][trait] << description.to_s
+      end
+    end
+  end
+  pigeon_list
 end
